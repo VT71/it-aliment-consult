49500c503b9949c371343d75a4236bcb71de5930
VT71 <56217276+VT71@users.noreply.github.com>
2023-11-15 18:53:16 +0000

    Description added for unit: Alimentatie Publica

--- src/lib/unitDescriptions.js
+++ src/lib/unitDescriptions.js
@@ -1,4 +1,17 @@
 const descriptions = {
+    alimentatiePublica: [
+        'Evaluarea unității sub aspectul respectării prevederilor legale din domeniul siguranței alimentelor și protecției consumatorului cu stabilirea neconformităților și consultarea Beneficiarului privind remedierea acestora cu emiterea unui raport la solicitare.',
+        'Asigurarea monitorizării periodice al unității în întregime cu toate sectoarele/părți componente sau separat.',
+        'Consultări la necesitate pe anumite subiecte separate.',
+        'Instruirea personalului  pe aspecte de igienă, aspecte tehnologice sau elaborarea unei tematici la necesitatea beneficiarului în dependență de locul de muncă a persoanei (spre ex. pe implementarea unui act normativ Lege,  Hotărâri de Guvern, Ordine sau Dispoziții emise de Autoritățile centrale). Modalitatea, tematica și periodicitatea, se discută cu beneficiarul, poate fi o dată pe an sau de două ori pe an, la fel se oferă instruiri pentru personalul nou angajat.',
+        'Elaborarea și implementarea procedurilor bazate pe principiile HACCP, sprijin permanent pentru menținerea acestor proceduri.',
+        'Elaborarea planului de autocontrol în cadrul întreprinderii.',
+        'Elaborarea programelor de control și combatere a dăunătorilor.',
+        'Elaborarea programelor de igienizare și dezinfecție în cadrul întreprinderii.',
+        'Elaborarea proiectelor tehnologice sau modificarea/ajustarea celor existente, consultări privind modificarea/ajustarea proiectelor.',
+        'Consultarea beneficiarului privind pregătirea unității și depunerea dosarului pentru primirea actului permisiv.',
+        'Colaborarea în baza contractului pe diverse aspecte stabilite.',
+    ],
     comertProduseAlimentare: [
         'Evaluarea unității sub aspectul respectării prevederilor legale din domeniul siguranței alimentelor și protecției consumatorului cu stabilirea neconformităților și consultarea Beneficiarului privind remedierea acestora cu emiterea unui raport la solicitare.',
         'Evaluarea la solicitarea beneficiarului, separat al unei părți componente din cadrul unei unități: secție de patiserie, secție de panificație, secție de tranșare carne, secție pește, secție de reambalare, sala de comerț etc. cu emiterea unui raport la solicitare.',
